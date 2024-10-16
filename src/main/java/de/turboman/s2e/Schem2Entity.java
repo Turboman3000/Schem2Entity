4f593749f944c964c5ceead8fe6573b6604a6f1c
Turboman3000 <turboman@turboman.de>
2024-10-16 21:49:42 +0200

    feat(Schem2Entity): refactor NBT handling for entity conversion

--- src/main/java/de/turboman/s2e/Schem2Entity.java
+++ src/main/java/de/turboman/s2e/Schem2Entity.java
@@ -2,16 +2,14 @@ package de.turboman.s2e;
 
 import dev.dewy.nbt.Nbt;
 import dev.dewy.nbt.tags.collection.CompoundTag;
-import net.minestom.server.MinecraftServer;
 import net.minestom.server.coordinate.Point;
 import net.minestom.server.entity.Entity;
 import net.minestom.server.instance.InstanceContainer;
-import net.minestom.server.registry.Registry;
+import net.minestom.server.item.Material;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Objects;
 
 public class Schem2Entity {
     private static final Nbt NBT = new Nbt();
@@ -37,9 +35,11 @@ public class Schem2Entity {
         for (short w = 0; w <= width; w++) {
             for (short h = 0; h <= height; h++) {
                 for (short l = 0; l <= length; l++) {
-                    final var dat = dataArray.get(w + h + l);
+                    final var material = Material.fromNamespaceId((String) palette.keySet().toArray()[dataArray.get(w + h + l)]);
 
-                    System.out.println(palette.keySet().toArray()[dat]);
+                    if (material == null) continue;
+
+                    System.out.println(material);
                 }
             }
         }
