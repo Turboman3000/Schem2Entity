b004cb7baafdd019b529d059bb683bbc3f80628e
Turboman3000 <turboman@turboman.de>
2024-10-18 16:44:43 +0200

    feat: Implemented schematic spawning with entity type and offset

--- src/main/java/de/turboman/s2e/Schem2Entity.java
+++ src/main/java/de/turboman/s2e/Schem2Entity.java
@@ -3,7 +3,10 @@ package de.turboman.s2e;
 import dev.dewy.nbt.Nbt;
 import dev.dewy.nbt.tags.collection.CompoundTag;
 import net.minestom.server.coordinate.Point;
+import net.minestom.server.coordinate.Pos;
 import net.minestom.server.entity.Entity;
+import net.minestom.server.entity.EntityType;
+import net.minestom.server.entity.metadata.display.BlockDisplayMeta;
 import net.minestom.server.instance.InstanceContainer;
 import net.minestom.server.item.Material;
 
@@ -14,9 +17,8 @@ import java.util.ArrayList;
 public class Schem2Entity {
     private static final Nbt NBT = new Nbt();
 
-    public static void spawn(File schematicFile, InstanceContainer instance, Point location) {
-        ArrayList<Entity> entitys = new ArrayList<>();
-
+    public static ArrayList<Entity> spawn(File schematicFile, InstanceContainer instance, Point location) {
+        final ArrayList<Entity> entities = new ArrayList<>();
         CompoundTag data;
 
         try {
@@ -25,6 +27,7 @@ public class Schem2Entity {
             throw new RuntimeException(e);
         }
 
+        final var offset = data.getIntArray("Offset");
         final var palette = data.getCompound("Blocks").getCompound("Palette");
         final var dataArray = data.getCompound("Blocks").getByteArray("Data");
 
@@ -32,16 +35,34 @@ public class Schem2Entity {
         final var height = data.getShort("Height").getValue();
         final var length = data.getShort("Length").getValue();
 
-        for (short w = 0; w <= width; w++) {
-            for (short h = 0; h <= height; h++) {
-                for (short l = 0; l <= length; l++) {
-                    final var material = Material.fromNamespaceId((String) palette.keySet().toArray()[dataArray.get(w + h + l)]);
+        for (short w = 0; w < width; w++) {
+            for (short h = 0; h < height; h++) {
+                for (short l = 0; l < length; l++) {
+                    final var paletteKey = (String) palette.keySet().toArray()[dataArray.get(w + l * width + h * width * length)];
+                    final var material = Material.fromNamespaceId(paletteKey);
+
+                    if (paletteKey.equals("minecraft:air")) continue;
+                    if (paletteKey.equals("minecraft:structure_void")) continue;
 
                     if (material == null) continue;
+                    if (material.block() == null) continue;
 
-                    System.out.println(material);
+                    final Entity entity = new Entity(EntityType.BLOCK_DISPLAY);
+                    final double finalX = w;
+                    final double finalY = h;
+                    final double finalZ = l;
+
+                    entity.editEntityMeta(BlockDisplayMeta.class, meta -> {
+                        meta.setBlockState(material.block());
+                        meta.setTranslation(new Pos(finalX, finalY, finalZ));
+                    });
+
+                    entity.setInstance(instance, location);
+                    entities.add(entity);
                 }
             }
         }
+
+        return entities;
     }
 }
