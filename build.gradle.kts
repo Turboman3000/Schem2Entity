f0b24648713de1d0479f7da1d55a6a1e33b01885
Turboman3000 <turboman@turboman.de>
2024-10-18 19:29:36 +0200

    feat: update build configuration and remove unneeded workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,9 @@
 plugins {
     id("java")
     id("com.github.johnrengelman.shadow") version "8.1.1"
-    id("maven-publish")
 }
 
-group = "de.turboman.s2e"
+group = "de.turboman"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -25,17 +24,4 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
-}
-
-publishing {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/Turboman3000/Schem2Entity")
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
-            }
-        }
-    }
 }
