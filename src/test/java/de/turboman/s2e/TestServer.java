f925d73cb6b6c8147838820fbdf7694d5240808b
Turboman3000 <turboman@turboman.de>
2024-10-18 16:44:21 +0200

    feat: Add SpawnCommand and PlayerSpawnEvent listener

--- src/test/java/de/turboman/s2e/TestServer.java
+++ src/test/java/de/turboman/s2e/TestServer.java
@@ -1,10 +1,12 @@
 package de.turboman.s2e;
 
-import de.turboman.s2e.Schem2Entity;
 import net.minestom.server.MinecraftServer;
+import net.minestom.server.command.CommandManager;
+import net.minestom.server.command.builder.Command;
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.event.GlobalEventHandler;
 import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
+import net.minestom.server.event.player.PlayerSpawnEvent;
 import net.minestom.server.instance.InstanceContainer;
 import net.minestom.server.instance.LightingChunk;
 import net.minestom.server.instance.block.Block;
@@ -12,23 +14,40 @@ import net.minestom.server.instance.block.Block;
 import java.io.File;
 
 public class TestServer {
+    static InstanceContainer instance;
+
     public static void main(String[] args) {
         MinecraftServer server = MinecraftServer.init();
         GlobalEventHandler eventHandler = MinecraftServer.getGlobalEventHandler();
-        InstanceContainer instance = MinecraftServer.getInstanceManager().createInstanceContainer();
+        CommandManager commandManager = MinecraftServer.getCommandManager();
+
+        instance = MinecraftServer.getInstanceManager().createInstanceContainer();
 
         instance.setChunkSupplier(LightingChunk::new);
         instance.setGenerator(unit -> unit.modifier().fillHeight(0, 40, Block.GRASS_BLOCK));
 
-        Schem2Entity.spawn(new File("./test.schem"),null,null);
-
         eventHandler.addListener(AsyncPlayerConfigurationEvent.class, e -> {
             e.setSpawningInstance(instance);
-            e.setClearChat(true);
-
             e.getPlayer().setRespawnPoint(new Pos(0, 40, 0));
         });
 
+        eventHandler.addListener(PlayerSpawnEvent.class, e -> {
+            e.getPlayer().setAllowFlying(true);
+        });
+
+        commandManager.register(new SpawnCommand());
+
         server.start("0.0.0.0", 25565);
+    }
+
+    static class SpawnCommand extends Command {
+
+        public SpawnCommand() {
+            super("spawn");
+
+            setDefaultExecutor((sender, args) -> {
+                Schem2Entity.spawn(new File("./test.schem"), instance, new Pos(0, 50, 0));
+            });
+        }
     }
 }
